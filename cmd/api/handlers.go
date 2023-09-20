fc1e2f8de627275c7ab800023624193d10d5b110
stilt0n <mattias.leino@arts.cornish.edu>
2023-09-19 23:47:26 -0400

    feat: add json utilty methods

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -17,31 +15,15 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "0.1.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	// Somewhat similar to `res.send`
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *application) AllPokemon(w http.ResponseWriter, r *http.Request) {
 	pokemon, err := app.Db.AllPokemon()
 	if err != nil {
-		fmt.Println(err)
+		app.errorJSON(w, err)
 		return
 	}
 
-	out, err := json.Marshal(pokemon)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, pokemon)
 }
